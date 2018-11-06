63e97948b8f494a010b2dfcd7f0c00f3065cf6ac
Sarah Townson <sarah.townson@scienceoxford.com>
2018-11-06 10:44:02 +0000

    Create morse_code.py

---
+++ week-2/morse_code.py
@@ -0,0 +1,18 @@
+from microbit import *
+import radio
+
+radio.on()
+
+while True:
+    if button_a.was_pressed() and button_b.was_pressed():
+        radio.send('     ')
+        display.show(Image.SQUARE)
+    elif button_a.was_pressed():
+        radio.send('dot ')
+        display.show(Image('00000:00000:00900:00000:00000'))
+    elif button_b.was_pressed():
+        radio.send('dash ')
+        display.show(Image('00000:00000:09990:00000:00000'))
+    else:
+        sleep(500)
+        display.clear()
