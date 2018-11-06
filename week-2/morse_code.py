2473a5df449da580ea92dbfa86382babe8af5302
Sarah Townson <sarah.townson@scienceoxford.com>
2018-11-06 15:18:02 +0000

    Update morse_code.py

--- week-2/morse_code.py
+++ week-2/morse_code.py
@@ -4,15 +4,17 @@ import radio
 radio.on()
 
 while True:
-    if accelerometer.was_gesture('up'):
+    if accelerometer.was_gesture('shake'):
         radio.send('     ')
         display.show(Image.SQUARE)
+        sleep(200)
     elif button_a.was_pressed():
         radio.send('dot ')
         display.show(Image('00000:00000:00900:00000:00000'))
+        sleep(200)
     elif button_b.was_pressed():
         radio.send('dash ')
         display.show(Image('00000:00000:09990:00000:00000'))
+        sleep(200)
     else:
-        sleep(500)
         display.clear()
