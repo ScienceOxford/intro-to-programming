d24c0938ef3f47cf2fedda28e8f85fbb07bd4a90
Scott Johnson <44942176+ScottPJohnson@users.noreply.github.com>
2018-11-20 19:40:18 +0000

    Update adventure_starter.py

--- week-4/adventure_starter.py
+++ week-4/adventure_starter.py
@@ -13,7 +13,7 @@ import sys
 def start():
     print("You wake up in a strange place.")
     print("On your left is a desk with a note on it, through the door you can see a street.")
-    print("To read the note, type READ.  To exit through the door, type LEAVE")
+    print("To read the note, type READ. To exit through the door, type LEAVE")
     while True:
         choice = input(">> ")
         if choice == "READ":
@@ -28,7 +28,7 @@ def start():
 def read():
     print("The note says: Come to the Red Wizard and ask for Sarah, she'll know what to do.")
     print("As you read the note, you hear a noise behind you.  You are not alone!")
-    print("To ask who is there, type ASK.  To rush and attack, type ATTACK.")
+    print("To ask who is there, type ASK. To rush and attack, type ATTACK.")
     while True:
         choice = input(">> ")
         if choice == "ASK":
@@ -38,17 +38,49 @@ def read():
         if choice == "QUIT":
             sys.exit()
         else:
-            print("Please choose ASK or ATTACK")
+            print("Please choose ASK or ATTACK, or you can QUIT")
     
 def leave():
-    print("You chose LEAVE")
+    print("After running, screaming, for a few hours you calm down sufficiently")
+    print("To return to the 'strange place', type START.")
+    while True:
+        choice = input(">> ")
+        if choice == "START":
+            return start()
+        if choice == "QUIT":
+            sys.exit()
+        else:
+            print("Please choose START, or you can QUIT")
 
 def ask():
-    print("You chose ASK")
+    print("You close your eyes tight and shout who's there?")
+    print("You hear the words you have always dreaded 'Deal or No Deal'... Its Edmonds!!")
+    print("To exit through the door, type LEAVE. To rush and attack, type ATTACK.")
+    while True:
+        choice = input(">> ")
+        if choice == "LEAVE":
+            return leave()
+        if choice == "ATTACK":
+            return attack()
+        if choice == "QUIT":
+            sys.exit()
+        else:
+            print("Please choose LEAVE or ATTACK, or you can QUIT")
 
 def attack():
-    print("You chose ATTACK")
-
+    print("You spin around to look you're attacker in the eye... Its Edmonds!")
+    print("You kick him in the 'Crinkley Bottom' and he scarpers")
+    print("The evil Edmonds is no more... You have successfully completed your adventure")
+    print("Type QUIT and go and have a cold drink!")
+    print("... or type START and take a different path through the adventure")
+    while True:
+        choice = input(">> ")
+        if choice == "START":
+            return start()
+        if choice == "QUIT":
+            sys.exit()
+        else:
+            print("Please choose QUIT, or you can START your adventure again")
 
 # The first few lines are instructions to your reader
 title = "**Choose Your Own Adventure**"
